7e96c866446a83287409e6e171d71e8dbf2b7634
Alexis Wurth <awurth.dev@gmail.com>
2017-11-08 16:23:27 +0100

    Added FieldErrors component

---
+++ src/components/util/FieldErrors.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="errors.length" class="ui error message">
+    <ul v-if="Array.isArray(errors) && errors.length > 1">
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+    <span v-if="Array.isArray(errors) && errors.length == 1">{{ errors[0] }}</span>
+    <span v-if="typeof errors == 'string'">{{ errors }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldErrors',
+  props: ['errors']
+}
+</script>
