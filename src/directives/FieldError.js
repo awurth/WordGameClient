08f5a114b5ef9d6a455ec208e66203d011a07e3a
Alexis Wurth <awurth.dev@gmail.com>
2017-11-09 13:01:49 +0100

    Improved FieldError directive

--- src/directives/FieldError.js
+++ src/directives/FieldError.js
@@ -1,13 +1,28 @@
 import Vue from 'vue'
+import _ from 'lodash'
+
+let valueHasErrors = value => {
+  return (_.isArray(value) && value.length) || !_.isEmpty(value)
+}
+
+let hasErrors = (value, arg) => {
+  if (arg && _.isPlainObject(value)) {
+    if (_.has(value, arg) && value[arg]) {
+      return valueHasErrors(value[arg])
+    }
+
+    return false
+  }
+
+  return valueHasErrors(value)
+}
 
 Vue.directive('field-error', {
   update: (el, binding) => {
-    if (Array.isArray(binding.value)) {
-      if (binding.value.length) {
-        window.$(el).addClass('error')
-      }
-    } else if (binding.value) {
+    if (hasErrors(binding.value, binding.arg)) {
       window.$(el).addClass('error')
+    } else {
+      window.$(el).removeClass('error')
     }
   }
 })
