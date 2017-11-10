fa31f141ef0557536ed4b45708c63da8adb46ddf
Alexis Wurth <awurth.dev@gmail.com>
2017-11-10 17:34:55 +0100

    Added action method to get a game's rounds

--- src/resources/Round.js
+++ src/resources/Round.js
@@ -1,5 +1,10 @@
 import Resource from '.'
 
-let Round = Resource('rounds{/id}')
+let Round = Resource('rounds{/id}', {}, {
+  ofGame: {
+    method: 'GET',
+    url: 'games/{id}/rounds'
+  }
+})
 
 export default Round
