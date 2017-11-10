5cbe2e7bc50ee9da2cdf87506ed7dfe32f2eeea5
Alexis Wurth <awurth.dev@gmail.com>
2017-11-10 16:58:43 +0100

    Improved label errors component (added direction)

--- src/components/util/LabelError.vue
+++ src/components/util/LabelError.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <div v-if="Array.isArray(errors) && errors.length == 1" class="ui pointing red basic label">{{ errors[0] }}</div>
-    <div v-else-if="typeof errors == 'string' && errors.length > 0" class="ui pointing red basic label">{{ errors }}</div>
+  <div v-if="errors && errors.length" :class="['ui', direction, 'pointing red basic label']">
+    <span v-if="Array.isArray(errors) && errors.length == 1">{{ errors[0] }}</span>
+    <span v-else-if="typeof errors == 'string' && errors.length > 0">{{ errors }}</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LabelError',
-  props: ['errors']
+  props: ['errors', 'direction']
 }
 </script>
